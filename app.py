fbbcdc7d58c171f194b0f395c2ba472277dbad5a
tp6yk6 <45422483+tp6yk6@users.noreply.github.com>
2018-12-06 19:36:19 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -31,59 +31,41 @@ def callback():
         abort(400)
     return 'OK'
 
-
-
-def keyword(text):
-    KeyWordDict = {"你好":"你也好啊"}
-    for k in KeyWordDict.keys():
-        if text.find(k)!=-1:
-            return[True,KeyWordDict[k]]
-    return[False]
 def Reply(event):
-    Ktemp = KeyWord(event.message.text)
-    if Ktemp[0]:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = Ktemp[1]))
-
-    else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text))
-
-def Button(event):
-    message = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-            thumbnail_image_url='https://example.com/image.jpg',
-            title='來算術吧',
-            text='1+1=?',
-            actions=[
-                PostbackTemplateAction(
-                    label='4',
-                    text='4',
-                    data=''
-                ),
-                PostbackTemplateAction(
-                    label='2',
-                    text='2',
-                    data=''
-                ),
-                PostbackTemplateAction(
-                    label='3',
-                    text='3',
-                    data=''
-                )
-            ]
+    return line_bot_api.reply_message(event.reply_token,
+        TemplateSendMessage(
+            alt_text='替代文字',
+            template=ButtonsTemplate(
+                thumbnail_image_url='圖案路徑.jpg',
+                title='標題',
+                text='內容',
+                actions=[
+                    PostbackTemplateAction(
+                        label='按鈕文字',
+                        text='發話文字',
+                        data='夾帶資料'
+                    ),
+                    MessageTemplateAction(
+                        label='按鈕文字',
+                        text='發話文字'
+                    ),
+                    URITemplateAction(
+                        label='按鈕文字',
+                        uri='網址'
+                    )
+                ]
+            )
         )
     )
-    line_bot_api.reply_message(event.reply_token, message)
-
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     try:
-        Button(event)
-        #Reply(event)
+        Reply(event)
     except Exception as e:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=str(e)))
+        line_bot_api.reply_message(event.reply_token, 
+            TextSendMessage(text=str(e)))
 
 import os
 if __name__ == "__main__":
