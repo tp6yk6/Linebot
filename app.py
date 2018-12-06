0c88708a8656dd05449d17f2fd850ced1146c1be
tp6yk6 <45422483+tp6yk6@users.noreply.github.com>
2018-12-06 19:12:20 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,61 +30,21 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
-
-def keyword(text):
-    KeyWordDict = {"你好":"你也好啊"}
-    for k in KeyWordDict.keys():
-        if text.find(k)!=-1:
-            return[True,KeyWordDict[k]]
-    return[False]
-def Reply(event):
-    Ktemp = KeyWord(event.message.text)
-    if Ktemp[0]:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = Ktemp[1]))
-
+def function(text):
+    list=['飯','麵','肉']
+    if text=='hi' or text=='Hi':
+        text='hello'
+    elif text=='餓':
+        text=random.choice(list)
     else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text = event.message.text))
-
-def Button(event):
-    message = TemplateSendMessage(
-    alt_text='Buttons template',
-    template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='來算術吧',
-        text='1+1=?',
-        actions=[
-            PostbackTemplateAction(
-                label='4',
-                text='4',
-                data=''
-            ),
-            PostbackTemplateAction(
-                label='2',
-                text='2',
-                data=''
-            ),
-            PostbackTemplateAction(
-                label='3',
-                text='3',
-                data=''
-            )
-            
-        ]
-    )
-)
-line_bot_api.reply_message(event.reply_token, message)
-
+        text='安安'
+    return text
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    try:
-        Button(event)
-        #Reply(event)
-    except Exception as e:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=str(e)))
+    message = TextSendMessage(function(event.message.text))
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
